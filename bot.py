8b86aaf7455a87a61a6c14fada7b5ee2d67926fb
pognog69 <pognog69@gmail.com>
2020-04-23 19:58:49 -0400

    the bot

--- bot.py
+++ bot.py
@@ -3,15 +3,17 @@ import requests
 import discord
 from dotenv import load_dotenv
 
-QUOTES_URL = "https://raw.githubusercontent.com/pognog69/tingfeng-discord/master/quotes.txt"
-quotesRequest = requests.get(QUOTES_URL, stream=True)
-quotes = []
-for line in quotesRequest.iter_lines(decode_unicode=True):
-    if line:
-        quotes.append(line)
-print(quotes)
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# client = discord.Client()
+QUOTES_URL = "https://raw.githubusercontent.com/pognog69/tingfeng-discord/master/quotes.txt"
+quotes = []
+for line in requests.get(QUOTES_URL, stream=True).iter_lines(decode_unicode=True):
+    if line: quotes.append(line)
+
+client = discord.Client()
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
