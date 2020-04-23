afec2ee82cc16663b6a4a73eb330383e907c8139
pognog69 <pognog69@gmail.com>
2020-04-23 19:53:53 -0400

    the quotes

--- bot.py
+++ bot.py
@@ -3,11 +3,15 @@ import requests
 import discord
 from dotenv import load_dotenv
 
+QUOTES_URL = "https://raw.githubusercontent.com/pognog69/tingfeng-discord/master/quotes.txt"
+quotesRequest = requests.get(QUOTES_URL, stream=True)
+quotes = []
+for line in quotesRequest.iter_lines(decode_unicode=True):
+    if line:
+        quotes.append(line)
+print(quotes)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-##client = discord.Client()
-
-
-print('h')
+# client = discord.Client()
